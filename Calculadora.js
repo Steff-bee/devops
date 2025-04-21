a12564dacd4b839f28c0db40485b52241f910497
stefanni <stefanni_luize2@hotmail.com>
2025-04-21 13:51:48 -0300

    ajustes
    ajustes para funcionamento

--- Calculadora.js
+++ Calculadora.js
@@ -1,26 +1,26 @@
-function calcular () {
-    const num1 = parseFloat(document.getElementById('num1').value)
-    const num2 = parseFloat(document.getElementById('num2').value)
-    const operador = document.getElementById('operador').value
-    let resultado
+function calcular() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operador = document.getElementById("operador").value;
+    let resultado;
   
     switch (operador) {
-      case '+':
-        resultado = num1 + num2
-        break
-      case '-':
-        resultado = num1 - num2
-        break
-      case '*':
-        resultado = num1 * num2
-        break
-      case '/':
-        resultado = num2 !== 0 ? num1 / num2 : 'Divisão por zero'
-        break
+      case "+":
+        resultado = num1 + num2;
+        break;
+      case "-":
+        resultado = num1 - num2;
+        break;
+      case "*":
+        resultado = num1 * num2;
+        break;
+      case "/":
+        resultado = num2 !== 0 ? num1 / num2 : "Divisão por zero";
+        break;
       default:
-        resultado = 'Operação inválido'
+        resultado = "Operação inválida";
     }
   
-    document.getElementById('resultado').textContent = `Resultado: ${resultado}`
+    document.getElementById("resultado").textContent = `Resultado: ${resultado}`;
   }
   
